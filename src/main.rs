ecb0109de0e45c58e41c89e25c749cf410561cf3
Hyuntae-Lee <kkonchi@gmail.com>
2017-05-29 19:37:41 +0900

    http request test

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,16 @@
+extern crate hyper;
+
+use hyper::client::Client;
+use std::io::Read;
 
 fn main() {
-    println!("hello");
+    let client = Client::new();
+
+    let mut res = client.get("http://finance.naver.com/item/main.nhn?code=063760").send().unwrap();
+
+    let mut bin_buff = Vec::new();
+
+    res.read_to_end(&mut bin_buff).unwrap();
+
+    println!("{:?}", bin_buff);
 }
