6f0fc2569a563f089ac4dd1ad21e765205dbb5a9
Hyuntae-Lee <kkonchi@gmail.com>
2017-06-01 19:39:07 +0900

    decode downloaded binary

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,60 @@
 extern crate hyper;
+extern crate html5ever;
+extern crate encoding;
 
 use hyper::client::Client;
-use std::io::Read;
+use html5ever::parse_document;
+use html5ever::rcdom::{RcDom};
+use html5ever::tendril::TendrilSink;
+
+use encoding::{Encoding, DecoderTrap};
+use encoding::all::WINDOWS_949;
+
+use std::io::{Read, Write};
+use std::fs::File;
 
 fn main() {
+    let mut buff = Vec::new();
+    if get_html_binary("063760", &mut buff) == 0 {
+        return;
+    }
+
+    // encoding 을 utf-8 로 변경
+    let decoded = match WINDOWS_949.decode(&buff[..], DecoderTrap::Replace) {
+        Err(_) => {
+            println!("Failed to decode");
+            return;
+        },
+        Ok(x) => x
+    };
+
+    println!("{}", decoded);
+}
+
+fn get_html_binary(code : &str, buff : &mut Vec<u8>) -> usize {
+    let base_url = "http://finance.naver.com/item/main.nhn?code=";
     let client = Client::new();
 
-    let mut res = client.get("http://finance.naver.com/item/main.nhn?code=063760").send().unwrap();
+    // url
+    let target_url = base_url.to_string() + code;
 
-    let mut bin_buff = Vec::new();
+    // send request to server
+    let mut resp = match client.get(&target_url).send() {
+        Err(x)  => {
+            println!("{:?}", x);
+            return 0;
+        },
+        Ok(x)   => x
+    };
 
-    res.read_to_end(&mut bin_buff).unwrap();
+    // read html binary
+    let size = match resp.read_to_end(buff) {
+        Err(x)  => {
+            println!("{:?}", x);
+            return 0;
+        },
+        Ok(x)   => x
+    };
 
-    println!("{:?}", bin_buff);
+    return size;
 }
