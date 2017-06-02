37143d04944413c94ec53fa3f0638d45c7f59354
Hyuntae-Lee <kkonchi@gmail.com>
2017-06-02 18:02:51 +0900

    코드 정리

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,10 @@ extern crate hyper;
 extern crate html5ever;
 extern crate encoding;
 
+use html5ever::parse_document;
+use html5ever::rcdom::{RcDom};
+use html5ever::tendril::TendrilSink;
+
 mod lib1;
 
 fn main() {
@@ -11,8 +15,10 @@ fn main() {
             println!("{}", x);
             return;
         },
-        Ok(x)   => x
+        Ok(x)   => String::from(x)
     };
+
+    let parser = parse_document(RcDom::default(), Default::default());
 
     println!("{}", html_str);
 }
