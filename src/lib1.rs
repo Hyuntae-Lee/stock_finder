6304ea4d1ba0b89993c19754d4f5f794608341e3
Hyuntae-Lee <kkonchi@gmail.com>
2017-06-09 19:45:43 +0900

    get values from single target

--- src/lib1.rs
+++ src/lib1.rs
@@ -1,64 +1,69 @@
 use hyper::client::Client;
+
 use html5ever::parse_document;
 use html5ever::rcdom::{RcDom, Handle, NodeEnum};
 use html5ever::tendril::TendrilSink;
+
 use encoding::{Encoding, DecoderTrap};
 use encoding::all::WINDOWS_949;
 
 use std::io::Read;
 
-pub fn collect_text_in_text_nodes(node_list : Vec<Handle>) -> Vec<String> {
-    let mut text_list : Vec<String> = Vec::new();
-
-    for handle in node_list {
-        let node = handle.borrow();
-        match node.node {
-            NodeEnum::Text(ref x)   => {
-                // get raw data
-                let raw_text = format!("{}", x);
-                let char_list = raw_text.chars();
-
-                // remove invalid chars
-                let mut text = String::new();
-                for c in char_list {
-                    if c != '\r' && c != '\n' && c != '\t' {
-                        text.push(c);
-                    }
-                }
-
-                // get valid data
-                if text.len() > 0 {
-                    text_list.push(text);
-                }
-            },
-            _                       => {}
-        };
-    }
-
-    text_list
+enum ValueItem {
+    NONE,
+    ROE,
+    PER,
+    PBR,
 }
 
-pub fn find_text_node(root : &Handle) -> Vec<Handle> {
-    let mut buffer : Vec<Handle> = Vec::new();
+pub fn get_value_with_code<'a>(code : &'a str) ->
+    Result<(Vec<f32>, Vec<f32>, Vec<f32>), &'a str> {
+    // get html string with code
+    let html_str = match get_page_html(code) {
+        Err(x)  => {
+            return Err(x);
+        },
+        Ok(x)   => String::from(x)
+    };
 
-    let node = root.borrow();
-    for child in &node.children {
-        let child_ref = child.borrow();
-        match child_ref.node {
-            NodeEnum::Text(_)   => {
-                buffer.push(child.clone());
-            },
-            _                       => {}
-        }
+    // parse html
+    let dom = match parse_html(html_str) {
+        Err(x)  => {
+            return Err(x);
+        },
+        Ok(x)   => x
+    };
 
-        let mut buff_for_child = find_text_node(child);
-        buffer.append(&mut buff_for_child);
-    }
+    // get values
+    let (roe_list, per_list, pbr_list) = match get_value_from_dom(dom) {
+        Err(x)  => {
+            return Err(x);
+        },
+        Ok(x)   => x
+    };
 
-    buffer
+    Ok((roe_list, per_list, pbr_list))
 }
 
-pub fn get_page_html<'a>(code : &str) -> Result<String, &'a str> {
+fn get_value_from_dom<'a>(dom : RcDom) ->
+    Result<(Vec<f32>, Vec<f32>, Vec<f32>), &'a str> {
+    // find text nodes
+    let text_node_list = find_text_node(&dom.document);
+    if text_node_list.len() == 0 {
+        return Err("Failed to collect nodes.");
+    }
+
+    // compose text list
+    let text_list = collect_text_in_text_nodes(text_node_list);
+    if text_list.len() == 0 {
+        return Err("Failed to collect texts.");
+    }
+
+    // get values
+    Ok(get_values_from_text_list(text_list))
+}
+
+fn get_page_html<'a>(code : &str) -> Result<String, &'a str> {
     let base_url = "http://finance.naver.com/item/main.nhn?code=";
     let client = Client::new();
     let mut buff = Vec::new();
@@ -96,7 +101,7 @@ pub fn get_page_html<'a>(code : &str) -> Result<String, &'a str> {
     return Ok(html_str);
 }
 
-pub fn parse_html<'a>(html_str: String) -> Result<RcDom, &'a str> {
+fn parse_html<'a>(html_str: String) -> Result<RcDom, &'a str> {
     let mut html_bytes = html_str.as_bytes();
     let parser = parse_document(RcDom::default(), Default::default());
     let decoder = parser.from_utf8();
@@ -109,4 +114,121 @@ pub fn parse_html<'a>(html_str: String) -> Result<RcDom, &'a str> {
     };
 
     return Ok(dom);
+}
+
+fn get_values_from_text_list(text_list : Vec<String>) ->
+    (Vec<f32>, Vec<f32>, Vec<f32>) {
+    let mut roe_list : Vec<f32> = Vec::new();
+    let mut per_list : Vec<f32> = Vec::new();
+    let mut pbr_list : Vec<f32> = Vec::new();
+    let mut item = ValueItem::NONE;
+
+    for text in text_list {
+        match item {
+            ValueItem::ROE => {
+                match text.parse::<f32>() {
+                    Ok(x)   => {
+                        roe_list.push(x);
+                        continue;
+                    },
+                    Err(_) => {
+                        item = text_to_item(&text);
+                    }
+                };
+            },
+            ValueItem::PER => {
+                match text.parse::<f32>() {
+                    Ok(x)   => {
+                        per_list.push(x);
+                        continue;
+                    },
+                    Err(_) => {
+                        item = text_to_item(&text);
+                    }
+                };
+            },
+            ValueItem::PBR => {
+                match text.parse::<f32>() {
+                    Ok(x)   => {
+                        pbr_list.push(x);
+                        continue;
+                    },
+                    Err(_) => {
+                        item = text_to_item(&text);
+                    }
+                };
+            },
+            _ => item = text_to_item(&text)
+        };
+    }
+
+    (roe_list, per_list, pbr_list)
+}
+
+fn text_to_item(text : &str) -> ValueItem {
+    if text == "ROE(%)" {
+        return ValueItem::ROE;
+    }
+    else if text == "PER(%)" {
+        return ValueItem::PER;
+    }
+    else if text == "PBR(배)" {
+        return ValueItem::PBR;
+    }
+    else {
+        return ValueItem::NONE;
+    }
+}
+
+fn collect_text_in_text_nodes(node_list : Vec<Handle>) -> Vec<String> {
+    let mut text_list : Vec<String> = Vec::new();
+
+    for handle in node_list {
+        let node = handle.borrow();
+        match node.node {
+            NodeEnum::Text(ref x)   => {
+                // get raw data
+                let raw_text = format!("{}", x);
+                let char_list = raw_text.chars();
+
+                // remove invalid chars
+                let mut text = String::new();
+                for c in char_list {
+                    if c != '\r' && c != '\n' && c != '\t' {
+                        text.push(c);
+                    }
+                }
+
+                // get valid data
+                if text.len() == 0 {
+                    continue;
+                }
+
+                text_list.push(text);
+            },
+            _                       => {}
+        };
+    }
+
+    text_list
+}
+
+fn find_text_node(root : &Handle) -> Vec<Handle> {
+    let mut buffer : Vec<Handle> = Vec::new();
+
+    let node = root.borrow();
+    for child in &node.children {
+        let child_ref = child.borrow();
+        match child_ref.node {
+            NodeEnum::Text(_)   => {
+                buffer.push(child.clone());
+            },
+            _                       => {}
+        }
+
+        let mut buff_for_child = find_text_node(child);
+        buffer.append(&mut buff_for_child);
+    }
+
+    buffer
 }
--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,8 @@ extern crate html5ever;
 mod lib1;
 
 fn main() {
-    // get html string with code
-    let html_str = match lib1::get_page_html("063760") {
-        Err(x)  => {
-            println!("{}", x);
-            return;
-        },
-        Ok(x)   => String::from(x)
-    };
 
-    // parse html
-    let dom = match lib1::parse_html(html_str) {
+    let (roe_list, per_list, pbr_list) = match lib1::get_value_with_code("063760") {
         Err(x)  => {
             println!("{}", x);
             return;
@@ -23,14 +14,8 @@ fn main() {
         Ok(x)   => x
     };
 
-    // find text nodes
-    let text_node_list = lib1::find_text_node(&dom.document);
-
-    // compose text list
-    let text_list = lib1::collect_text_in_text_nodes(text_node_list);
-
     // print output - debug
-    for text in text_list {
-        println!("{}", text);
-    }
+    println!("ROE : {:?}", roe_list);
+    println!("PER : {:?}", per_list);
+    println!("PBR : {:?}", pbr_list);
 }
