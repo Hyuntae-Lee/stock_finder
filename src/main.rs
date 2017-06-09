26b81727c53b7eb4a7a29de46195ce73bc0fbff9
Hyuntae-Lee <kkonchi@gmail.com>
2017-06-09 19:48:55 +0900

    lib1 -> value_getter

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,17 @@ extern crate hyper;
 extern crate encoding;
 extern crate html5ever;
 
-mod lib1;
+mod value_getter;
 
 fn main() {
 
-    let (roe_list, per_list, pbr_list) = match lib1::get_value_with_code("063760") {
-        Err(x)  => {
-            println!("{}", x);
-            return;
-        },
-        Ok(x)   => x
+    let (roe_list, per_list, pbr_list) =
+        match value_getter::get_value_with_code("063760") {
+            Err(x)  => {
+                println!("{}", x);
+                return;
+            },
+            Ok(x)   => x
     };
 
     // print output - debug
